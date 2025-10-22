734a6f9e61696e063c3dff3857dbf830284c97d5
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-22 15:27:06 +0800

    financing

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import { getLastPath, injectCSS } from "./src/utils.js"
+
 import { copyTradingFutures } from "./src/js/copyTradingFutures.js";
 import { appDownload } from "./src/js/appDownload.js";
+import { financing } from "./src/js/financing.js";
+
 import appDownload_css from './src/css/appDownload.css';
 
 const page = getLastPath();
@@ -12,6 +15,10 @@ switch (page) {
     case 'appDownload':
         appDownload();
         injectCSS(appDownload_css);
+        break;
+
+    case 'financing':
+        financing();
         break;
 
     default:
