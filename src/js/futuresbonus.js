24aa73ad7548139f0f2627294d7c186eef522814
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-30 16:11:23 +0800

    wait for banner

--- src/js/futuresbonus.js
+++ src/js/futuresbonus.js
@@ -1,15 +1,13 @@
 import { waitFor, isDark } from '../utils.js';
 
 export function futuresbonus() {
-    waitFor('#app')
-    .then(() => {
+    waitFor('.futuresbonus .banner')
+    .then((banner) => {
         const darkUrl = 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f93950f4ee344fdab49401ac27bd7252.png';
         const lightUrl = '';
         const imgUrl = isDark() ? darkUrl : darkUrl;
-        const bannerImg = document.querySelector('.futuresbonus .banner');
-        if (!bannerImg) return;
-        bannerImg.style.backgroundImage = `url(${imgUrl})`;
-        bannerImg.style.backgroundPosition = '80% center';
-        bannerImg.style.backgroundSize = 'auto';
+        banner.style.backgroundImage = `url(${imgUrl})`;
+        banner.style.backgroundPosition = '80% center';
+        banner.style.backgroundSize = 'auto';
     }).catch(err => console.error(err));
 }
