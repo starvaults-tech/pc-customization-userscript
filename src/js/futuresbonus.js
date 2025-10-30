81901068d6869061fa6b07726ceca084d273fd86
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-30 16:15:20 +0800

    using observer to change the image

--- src/js/futuresbonus.js
+++ src/js/futuresbonus.js
@@ -1,13 +1,20 @@
 import { waitFor, isDark } from '../utils.js';
 
 export function futuresbonus() {
-    waitFor('.futuresbonus .banner')
-    .then((banner) => {
-        const darkUrl = 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f93950f4ee344fdab49401ac27bd7252.png';
-        const lightUrl = '';
-        const imgUrl = isDark() ? darkUrl : darkUrl;
-        banner.style.backgroundImage = `url(${imgUrl})`;
+    const darkUrl = 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f93950f4ee344fdab49401ac27bd7252.png';
+    const lightUrl = '';
+    const getUrl = () => (isDark() ? darkUrl : darkUrl);
+    const applyStyle = (banner) => {
+        banner.style.backgroundImage = `url(${getUrl()})`;
         banner.style.backgroundPosition = '80% center';
         banner.style.backgroundSize = 'auto';
-    }).catch(err => console.error(err));
+    };
+    waitFor('.futuresbonus .banner').then(banner => {
+        applyStyle(banner);
+        const observer = new MutationObserver(() => {
+            if (banner.style.backgroundImage.includes(getUrl())) return;
+            applyStyle(banner);
+        });
+        observer.observe(banner, { attributes: true, attributeFilter: ['style'] });
+    });
 }
