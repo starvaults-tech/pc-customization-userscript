6171fb9ea4d30c4d4adbc4f67cfdf9007554aac5
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-30 16:31:00 +0800

    login guide image

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+import { waitFor, isDark } from '../utils.js';
+
+export function gridStrategy() {
+    waitFor('.guide-info img')
+    .then(() => {
+        const darkUrl  = 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f7db93e71a09624cd9a482a28fc9f58b.png';
+        const lightUrl = '';
+        const imgUrl   = isDark() ? darkUrl : darkUrl;
+        const guideImg  = document.querySelector('.guide-info img');
+        if (!guideImg) return;
+        guideImg.src = imgUrl;
+    }).catch(err => console.error(err));
+}
