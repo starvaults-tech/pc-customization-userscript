4145ced150dfb71f9b2745d17845d7e73cc0b92a
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-30 14:26:13 +0800

    wait for element

--- src/js/gridStrategy.js
+++ src/js/gridStrategy.js
@@ -1,11 +1,14 @@
-import { isDark } from '../utils.js';
+import { waitFor, isDark } from '../utils.js';
 
 export function gridStrategy() {
-    const ImgUrl = {
-        dark:  'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f3589eeca5eae7efc454f9dc8712318b.png',
-        light: 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/43ef4cc9c6c7680c88f6f18fb670eb80.png'
-    };
-    const headImg = document.querySelector('.strategy-head > img');
-    if (!headImg) return;
-    headImg.src = isDark() ? ImgUrl.dark : ImgUrl.light;
+    waitFor('#app')
+    .then(() => {
+        const ImgUrl = {
+            dark:  'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/f3589eeca5eae7efc454f9dc8712318b.png',
+            light: 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/43ef4cc9c6c7680c88f6f18fb670eb80.png'
+        };
+        const headImg = document.querySelector('.strategy-head > img');
+        if (!headImg) return;
+        headImg.src = isDark() ? ImgUrl.dark : ImgUrl.light;
+    }).catch(err => console.error(err));
 }
--- src/utils.js
+++ src/utils.js
@@ -11,6 +11,28 @@ export function isDark() {
     return document.querySelector('#app')?.classList?.contains('dark-theme');
 }
 
+export function waitFor(selector, timeout = 10000) {
+    return new Promise((resolve, reject) => {
+        const element = document.querySelector(selector);
+        if (element) return resolve(element);
+
+        const observer = new MutationObserver(() => {
+            const el = document.querySelector(selector);
+            if (el) {
+                observer.disconnect();
+                resolve(el);
+            }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+
+        setTimeout(() => {
+            observer.disconnect();
+            reject(new Error(`Timeout: Element ${selector} not found within ${timeout}ms`));
+        }, timeout);
+    });
+}
+
 export function injectCSS(css) {
     const style = document.createElement('style');
     style.textContent = css;
