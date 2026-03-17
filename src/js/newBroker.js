c3077e1a1ea824e58d78f895e7821dfb6bea210a
Hsin <joanne45644@gmail.com>
2026-03-17 20:27:43 +0800

    style(newbroker): update broker advantages section pic with light theme

--- src/js/newBroker.js
+++ src/js/newBroker.js
@@ -1,18 +1,33 @@
 import { isDark, waitFor } from '../utils.js'
 
 export function newBroker() {
-    const ImgUrls = [
-        'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/fe6785c2c8fb4af5c851f34e0e2a60a6.png',
-        'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/ba627c0842c47fca04309d8597ae5afd.png',
-        'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/fd6c09f61253ec29511f71963d963b7c.png'
-    ];
-    const applyImage = () => {
-        const imgs = document.querySelectorAll('.newBroker-features-item .newBroker-features-img');
-        imgs.forEach((item, index) => { item.src = ImgUrls[index]; item.style.animationDelay = `${index * 2}s`; })
-    }
-    waitFor('#app').then(() => {
+    waitFor('#app').then((appDom) => {
         waitFor('.newBroker-features-item .newBroker-features-img').then(() => {
-            applyImage();
+            const ImgUrls = {
+                dark: [
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/fe6785c2c8fb4af5c851f34e0e2a60a6.png',
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/ba627c0842c47fca04309d8597ae5afd.png',
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/fd6c09f61253ec29511f71963d963b7c.png'
+                ],
+                light: [
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/554011ad2d9f29bfcbf20317161a811d.png',
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/7ae0cbcb56479d941d2b0dfb94505f05.png',
+                    'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/92486481f29811e1bc7cf82c7cf87f40.png'
+                ]
+            };
+            const updateFeatureImgs = () => {
+                const imgs = document.querySelectorAll('.newBroker-features-item .newBroker-features-img');
+                imgs.forEach((item, index) => {
+                    const url = isDark() ? ImgUrls.dark[index] : ImgUrls.light[index];
+                    item.setAttribute('src', url);
+                    setTimeout(() => { if (item.src !== url) item.setAttribute('src', url); }, 100);
+                    item.style.animationDelay = `${index * 2}s`;
+                });
+            };
+            updateFeatureImgs();
+            setTimeout(updateFeatureImgs, 100);
+            setTimeout(updateFeatureImgs, 300);
+            new MutationObserver(updateFeatureImgs).observe(appDom, { attributes: true, attributeFilter: ['class'] });
         }).catch(err => console.error(err));
  // banner 背景（僅深色模式）
         const darkUrl = 'https://saas2-s3-public-01.s3.ap-northeast-1.amazonaws.com/1835/upload/b65044880ab7f0ffe85caef435947569.jpg';
